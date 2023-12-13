774fa30e1c39e908312771630880cb0e1944dd75
Drake Davis <drakedavis0203@gmail.com>
2023-12-12 22:38:34 -0500

    Implement RandomForestClassifier from scikit-learn for binary classification of thermal runaway risks in EV batteries. Include data generation for synthetic testing. Use classification report for model accuracy assessment. Prepare script structure for further development and integration with real dataset.

--- EV_PyTorch.py
+++ EV_PyTorch.py
@@ -1,1 +1,26 @@
-# First commit via Linux
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+
+# 100 samples with temperature, voltage, current; binary target
+x = np.random.rand(100, 3)  
+y = np.random.randint(2, size=100)
+
+print(x)
+print(y)
+
+# # Split the dataset into training and testing sets
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+# # Initialize the Random Forest model 
+model = RandomForestClassifier()
+
+# # Train the model
+model.fit(x_train, y_train)
+
+# Predict on the test set
+y_pred = model.predict(x_test)
+
+# Evaluate the model
+print(classification_report(y_test, y_pred))
